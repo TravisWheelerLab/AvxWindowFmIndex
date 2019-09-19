6c93067a1dc550ac501bfb2a447649cbb8784846
Tim Anderson <tim.anderson@umontana.edu>
2019-09-19 15:17:22 -0600

    remove null terminator check, add in check for X ambiguity character

--- AwFmLetter.c
+++ AwFmLetter.c
@@ -1,12 +1,8 @@
 #include "AwFmLetter.h"
 #include <stdlib.h>
 
-frequencyIndexedLetter_t awFmAsciiLetterToLetterIndex(char asciiLetter){
 
-  //handle $ null terminator
-  if(asciiLetter == '$'){
-    asciiLetter = 0;
-  }
+frequencyIndexedLetter_t awFmAsciiLetterToLetterIndex(char asciiLetter){
 
   //bitmask to alias lowercase letter to uppercase letters
   const char charBitmask = 0x1F;
@@ -29,5 +25,14 @@ frequencyIndexedLetter_t awFmAsciiLetterToLetterIndex(char asciiLetter){
   else if(asciiLetter == ('Z' & charBitmask)){
     asciiLetter = (rand() & 1)? 'E'& charBitmask: 'Q'& charBitmask;
   }
+  else if(asciiLetter == 'X' & charBitmask){
+    //X is the ambiguity character for completely unknown, randomize the letter.
+    asciiLetter = (rand() % 19) + 1;
+    //if we randomized to another illegal character, add 1 to get a legal char.
+    if(letterOffsets[asciiLetter] == 21){
+      asciiLetter += 1;
+    }
+  }
+  
   return letterOffsets[asciiLetter];
 }

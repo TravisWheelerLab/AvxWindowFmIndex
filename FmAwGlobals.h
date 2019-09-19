5e88f10f7df58b3a093338829dba1c1e4d18f4bb
Tim Anderson <tim.anderson@umontana.edu>
2019-09-18 18:39:40 -0600

    Add project global definitions

---
+++ FmAwGlobals.h
@@ -0,0 +1,7 @@
+#ifndef FM_AW_INDEX_GLOBALS_H
+#define FM_AW_INDEX_GLOBALS_H
+
+#define CACHE_LINE_SIZE_IN_BYTES  64
+#define POSITIONS_PER_FM_BLOCK    7
+
+#endif /* end of include guard: FM_AW_INDEX_GLOBALS_H */

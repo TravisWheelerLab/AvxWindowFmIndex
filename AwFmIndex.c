4cbf155c2bf48918d674e61b479a360413358311
Tim Anderson <tim.anderson@umontana.edu>
2019-09-23 17:11:37 -0600

    removed suffixArray from index struct, added SA compression, commented functions

--- AwFmIndex.c
+++ AwFmIndex.c
@@ -2,34 +2,88 @@
 #include "FmAwGlobals.h"
 
 
+//TODO: when creating rankPrefixSums, make sure that the prefix sum for A ia 1 (for the $ terminator)
+
+/*
+ * Function:  alignedAllocAwFmIndex
+ * --------------------
+ * Dynamically allocates memory for the AwFmIndex struct, aligned to a cache line (64 bytes).
+ *
+ *  Returns:
+ *    Pointer to the allocated aligned memory. As per C11 specs, will return NULL on allocation failure.
+ */
 struct AwFmIndex *alignedAllocAwFmIndex(void){
   return aligned_alloc(CACHE_LINE_SIZE_IN_BYTES, sizeof(struct FmIndex));
 }
 
+
+/*
+ * Function:  alignedAllocBlockList
+ * --------------------
+ * Dynamically allocates memory for the AwFmBlock array, aligned to a cache line (64 bytes).
+ *
+ *  Inputs:
+ *    numBlocks: size of the allocated AwFmBlock array.
+ *
+ *  Returns:
+ *    Pointer to the allocated aligned memory. As per C11 specs, will return NULL on allocation failure.
+ */
 struct AwFmBlock *alignedAllocBlockList(const size_t numBlocks){
   return aligned_alloc(CACHE_LINE_SIZE_IN_BYTES, numBlocks * sizeof(FM_block));
 
 }
 
-size_t *allocAndInitFullTextPositionLookupTable(const size_t numPositions){
-  const size_t *positionList = malloc(numPositions * sizeof(size_t));
-  for(size_t i = 0; i< numPositions; i++){
-    positionList[i] = i;
-  }
 
-  return positionList;
-}
-
+/*
+ * Function:  deallocateFmIndex
+ * --------------------
+ * Frees the memory associated with the given AwFmIndex. This also deallocates the block list.
+ *
+ *  Inputs:
+ *    index: Index struct to be deallocated.
+ */
 void deallocateFmIndex(struct FmIndex *restrict index){
   if(index != NULL){
     if(index->blockList != NULL){
       free(index->blockList)
     }
 
-    if(index->fullTextPositionLookupTable != NULL){
-      free(index->fullTextPositionLookupTable)
-    }
-    
     free(index);
   }
+}
+
+/*
+ * Function:  awFmGetBwtLength
+ * --------------------
+ * Gets the length of the BWT from the AwFmIndex data structure.
+ *   The AwFmIndex stores the BWT length at the end of the rankPrefixSums array.
+ *
+ *  Inputs:
+ *    index: AwFmIndex struct to query.
+ *
+ *  Outputs:
+*     length of the BWT array for the given AwFmIndex.
+ */
+uint64_t awFmGetBwtLength(const struct AwFmIndex *restrict const index){
+  //the bwt length is stored in the prefix sums after all the actual amino acid prefix sums.
+  //conceptually, the bwt length == prefix sum for a character after every alphabet letter.
+  return index->rankPrefixSums[AMINO_CARDINALITY];
+}
+
+/*
+ * Function:  awFmGetDbSequenceLength
+ * --------------------
+ * Gets the length of the database sequence from the AwFmIndex data structure.
+ *   Since the database sequence doesn't contain a null terminator ($), it is
+ *   one less than the BWT length.
+ *
+ *  Inputs:
+ *    index: AwFmIndex struct to query.
+ *
+ *  Outputs:
+ *    length of the database sequence for the given AwFmIndex.
+ */
+//the database sequence contains every character from the bwt, minus the null terminator $.
+uint64_t awFmGetDbSequenceLength(const struct AwFmIndex *restrict const index){
+  return awFmGetBwtLength(index) - 1;
 }
--- AwFmIndex.h
+++ AwFmIndex.h
@@ -11,7 +11,7 @@ struct AwFmBlock{
 
 struct AwFmIndexMetadata{
   uint32_t  versionNumber;
-  uint8_t   IArrayPositionWidth;
+  uint16_t  suffixArrayCompressionRatio;
 };
 
 union AwFmIndexPaddedMetadata{
@@ -21,20 +21,21 @@ union AwFmIndexPaddedMetadata{
 
 struct AwFmIndex{
   struct AwFmBlock *blockList;
-  uint64_t numBlocks;
-  uint64_t rankPrefixSums[AMINO_CARDINALITY + 1]; //last position acts as BWT length
-  uint64_t nullTerminatorPosition;
-  size_t *fullTextPositionLookupTable;
+  uint64_t  numBlocks;
+  uint64_t  rankPrefixSums[AMINO_CARDINALITY + 1]; //last position acts as BWT length
   union AwFmIndexPaddedMetadata metadata;
 };
 
 
 /*Public Functions*/
+
 //API Function
 struct  AwFmIndex *alignedAllocAwFmIndex(void);
 //API Function
 struct  AwFmBlock *alignedAllocBlockList(const size_t numBlocks);
-        size_t    *allocAndInitFullTextPositionLookupTable(const size_t numPositions);
         void      deallocateFmIndex(const struct FmIndex *restrict const index);
+
+uint64_t awFmGetBwtLength(const struct AwFmIndex *restrict const index);
+uint64_t awFmGetDbSequenceLength(const struct AwFmIndex *restrict const index);
 
 #endif /* end of include guard: AW_FM_INDEX_STRUCTS_H */

f45860b57097e5401cc87bbff995dcae3dfa51c8
Tim Anderson <tim.anderson@umontana.edu>
2019-09-23 17:00:35 -0600

    add global definition file

---
+++ AwFmGlobals.h
@@ -0,0 +1,8 @@
+#ifndef AW_FM_INDEX_GLOBALS_H
+#define AW_FM_INDEX_GLOBALS_H
+
+#define CACHE_LINE_SIZE_IN_BYTES  64
+#define POSITIONS_PER_FM_BLOCK    7
+#define AMINO_CARDINALITY         20
+
+#endif /* end of include guard: AW_FM_INDEX_GLOBALS_H */

bb551bc99a437ac85e13075c2c98e1ababa15bf4
Tim Anderson <tim.anderson@umontana.edu>
2019-09-26 17:44:08 -0600

    add fileSrc to index, move suffixArrayCompression out of metadata, add alloc, dealloc, and helper functions
    Index now stores the fileSrc, to keep an open index coupled to the file 
    that it represents.
    suffixArrayCompressionRatio isn't really a metadata field, so it makes 
    more sense to belong in the AwFmIndex struct.
    
    This commit also adds AwFmSearchRange, to represent a (start pointer - 
    end pointer) range in the BWT, helpful for returning kmer suffix matches 
    from functions.

--- AwFmIndex.c
+++ AwFmIndex.c
@@ -1,6 +1,7 @@
-#include "FmIndex.c"
-#include "FmAwGlobals.h"
-
+#include "AwFmIndex.h"
+#include "AwFmGlobals.h"
+#include <string.h>
+#include <stdlib.h>
 
 //TODO: when creating rankPrefixSums, make sure that the prefix sum for A ia 1 (for the $ terminator)
 
@@ -8,12 +9,39 @@
  * Function:  alignedAllocAwFmIndex
  * --------------------
  * Dynamically allocates memory for the AwFmIndex struct, aligned to a cache line (64 bytes).
+ *  Inputs:
+ *    fileSrc:    Null-terminated string representing the absolute location
+ *      of the file to read index from, or write index to.
+ *      If for some reason file reading/writing is not needed, fileSrc may be left NULL.
+ *      This string will be copied into a dynamically allocated string, and the AwFmIndex struct
+ *      will be responsible for the lifetime of this copy.
  *
  *  Returns:
- *    Pointer to the allocated aligned memory. As per C11 specs, will return NULL on allocation failure.
+ *    Pointer to the allocated aligned memory. Returns NULL on allocation failure.
  */
-struct AwFmIndex *alignedAllocAwFmIndex(void){
-  return aligned_alloc(CACHE_LINE_SIZE_IN_BYTES, sizeof(struct FmIndex));
+struct AwFmIndex *alignedAllocAwFmIndex(const char *restrict const fileSrc){
+  struct AwFmIndex *restrict index = aligned_alloc(CACHE_LINE_SIZE_IN_BYTES, sizeof(struct AwFmIndex));
+  if(index != NULL){
+
+    if(fileSrc != NULL){
+      //memchr acts as strlen, but won't crash if the null terminator isn't reached.
+      //GCC doesn't seem to find strnlen, so this will do instead.
+      const void *nullTerminatorPosition = memchr(fileSrc, 0, MAXIMUM_FILE_PATH_LENGTH);
+      if(nullTerminatorPosition != NULL){
+
+        //allocate a copy of the fileSrc string, and copy it over (null terminator included)
+        const size_t fileSrcLength = (char*)nullTerminatorPosition - fileSrc;
+        index->fileSrc = malloc((fileSrcLength + 1) * sizeof(char));
+        memcpy(index->fileSrc, fileSrc, fileSrcLength);
+      }
+      //if there was no null terminator in the fileSrc (UNLIKELY), set fileSrc to null.
+      else{
+        index->fileSrc = NULL;
+      }
+    }
+  }
+
+  return index;
 }
 
 
@@ -29,7 +57,7 @@ struct AwFmIndex *alignedAllocAwFmIndex(void){
  *    Pointer to the allocated aligned memory. As per C11 specs, will return NULL on allocation failure.
  */
 struct AwFmBlock *alignedAllocBlockList(const size_t numBlocks){
-  return aligned_alloc(CACHE_LINE_SIZE_IN_BYTES, numBlocks * sizeof(FM_block));
+  return aligned_alloc(CACHE_LINE_SIZE_IN_BYTES, numBlocks * sizeof(struct AwFmBlock));
 
 }
 
@@ -42,14 +70,30 @@ struct AwFmBlock *alignedAllocBlockList(const size_t numBlocks){
  *  Inputs:
  *    index: Index struct to be deallocated.
  */
-void deallocateFmIndex(struct FmIndex *restrict index){
+void deallocateFmIndex(struct AwFmIndex *restrict index){
+
   if(index != NULL){
+    if(index->fileSrc != NULL){
+      free(index->fileSrc);
+    }
     if(index->blockList != NULL){
-      free(index->blockList)
+      free(index->blockList);
     }
 
     free(index);
   }
+}
+
+
+
+//todo comment
+inline size_t awFmSearchRangeLength(const struct AwFmSearchRange *restrict const range){
+  return (range->endPtr - range->startPtr) + 1;
+}
+
+
+inline bool awFmBwtPositionIsSampled(const struct AwFmIndex *restrict const index, const uint64_t position){
+  return position % index->suffixArrayCompressionRatio;
 }
 
 /*
@@ -64,7 +108,7 @@ void deallocateFmIndex(struct FmIndex *restrict index){
  *  Outputs:
 *     length of the BWT array for the given AwFmIndex.
  */
-uint64_t awFmGetBwtLength(const struct AwFmIndex *restrict const index){
+inline uint64_t awFmGetBwtLength(const struct AwFmIndex *restrict const index){
   //the bwt length is stored in the prefix sums after all the actual amino acid prefix sums.
   //conceptually, the bwt length == prefix sum for a character after every alphabet letter.
   return index->rankPrefixSums[AMINO_CARDINALITY];
@@ -84,6 +128,29 @@ uint64_t awFmGetBwtLength(const struct AwFmIndex *restrict const index){
  *    length of the database sequence for the given AwFmIndex.
  */
 //the database sequence contains every character from the bwt, minus the null terminator $.
-uint64_t awFmGetDbSequenceLength(const struct AwFmIndex *restrict const index){
+inline uint64_t awFmGetDbSequenceLength(const struct AwFmIndex *restrict const index){
   return awFmGetBwtLength(index) - 1;
+}
+
+
+inline uint64_t awFmGetCompressedSuffixArrayLength(const struct AwFmIndex *restrict const index){
+  return awFmGetBwtLength(index) / index->suffixArrayCompressionRatio;
+}
+
+
+/*
+ * Function:  awFmSearchRangeIsValid
+ * --------------------
+ * Compares the start pointer and end pointer to determine if this range
+ *  contains a valid range of the relevant kmer suffix.
+ *
+ *  Inputs:
+ *    searchRange: AwFmSearchRange struct to compare.
+ *
+ *  Outputs:
+ *    True if this range represents a range of positions for the given kmer suffix in the database,
+ *      or false if the database does not contain the given kmer suffix.
+ */
+inline bool awFmSearchRangeIsValid(const struct AwFmSearchRange *restrict const searchRange){
+  return searchRange->startPtr < searchRange->endPtr;
 }
--- AwFmIndex.h
+++ AwFmIndex.h
@@ -2,6 +2,12 @@
 #define AW_FM_INDEX_STRUCTS_H
 
 #include <stdint.h>
+#include <stdbool.h>
+#include <immintrin.h>
+#include "AwFmGlobals.h"
+
+
+#define AW_FM_INDEX_METADATA_BYTE_SIZE 64
 
 /*Structs*/
 struct AwFmBlock{
@@ -11,31 +17,39 @@ struct AwFmBlock{
 
 struct AwFmIndexMetadata{
   uint32_t  versionNumber;
-  uint16_t  suffixArrayCompressionRatio;
 };
 
 union AwFmIndexPaddedMetadata{
   struct AwFmIndexMetadata data;
-  uint8_t padding[FM_INDEX_METADATA_BYTE_SIZE];
+  uint8_t padding[AW_FM_INDEX_METADATA_BYTE_SIZE];
 };
 
 struct AwFmIndex{
   struct AwFmBlock *blockList;
   uint64_t  numBlocks;
+  uint16_t  suffixArrayCompressionRatio;
   uint64_t  rankPrefixSums[AMINO_CARDINALITY + 1]; //last position acts as BWT length
+  char      *fileSrc;
   union AwFmIndexPaddedMetadata metadata;
+};
+
+struct AwFmSearchRange{
+  uint64_t startPtr;
+  uint64_t endPtr;
 };
 
 
 /*Public Functions*/
 
 //API Function
-struct  AwFmIndex *alignedAllocAwFmIndex(void);
-//API Function
+struct  AwFmIndex *alignedAllocAwFmIndex(const char *restrict const fileSrc);
 struct  AwFmBlock *alignedAllocBlockList(const size_t numBlocks);
-        void      deallocateFmIndex(const struct FmIndex *restrict const index);
-
-uint64_t awFmGetBwtLength(const struct AwFmIndex *restrict const index);
-uint64_t awFmGetDbSequenceLength(const struct AwFmIndex *restrict const index);
+        void      deallocateFmIndex(struct AwFmIndex *restrict index);
+inline  bool      awFmBwtPositionIsSampled(const struct AwFmIndex *restrict const index, const uint64_t position);
+inline  size_t    awFmSearchRangeLength(const struct AwFmSearchRange *restrict const range);
+inline  uint64_t  awFmGetBwtLength(const struct AwFmIndex *restrict const index);
+inline  uint64_t  awFmGetDbSequenceLength(const struct AwFmIndex *restrict const index);
+inline  uint64_t  awFmGetCompressedSuffixArrayLength(const struct AwFmIndex *restrict const index);
+inline bool       awFmSearchRangeIsValid(const struct AwFmSearchRange *restrict const searchRange);
 
 #endif /* end of include guard: AW_FM_INDEX_STRUCTS_H */

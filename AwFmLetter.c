8062f1ff1256dfdf2149ef8b6ffdfbe498143ed1
Tim Anderson <tim.anderson@umontana.edu>
2019-09-26 17:47:05 -0600

    change input types to uint8_t, add transform from vector letter to freq index letter.
    The input types were changed to uint8_t because the C standard doesn't 
    define whether char is signed or not, so it ~might~ index with a 
    negative value, causing weird shit. This should not modify the input 
    characters at all, just how they're used to index.
    
    In order to do the backtrace to determine database sequence position, we 
    need a function to convert letters in the AwFmBlock vectors back to 
    frequency-indexed.

--- AwFmLetter.c
+++ AwFmLetter.c
@@ -1,7 +1,5 @@
 #include "AwFmLetter.h"
-
-
-
+#include <stdlib.h>
 /*
  * Function:  awFmAsciiLetterToLetterIndex
  * --------------------
@@ -16,14 +14,14 @@
  *    Frequency-encoded value representing the amino acid.
  *
  */
-uint8_t awFmAsciiLetterToLetterIndex(char asciiLetter){
+uint8_t awFmAsciiLetterToLetterIndex(uint8_t asciiLetter){
   //bitmask to alias lowercase letter to uppercase letters
   const char charBitmask = 0x1F;
   asciiLetter = asciiLetter & charBitmask;
 
   //frequency indexed letter lookup table. values of 21 are illegal characters, and should not match to anything.
   //by frequency, L,A,G,V,E,S,I,K,R,D,T,P,N,Q,F,Y,M,H,C,W
-  const letter_t letterOffsets[32] = {
+  const uint8_t letterOffsets[32] = {
     //letters A - Z
     21, 1,21,18, 9, 4,14, 2,
     17, 6,21, 7, 0,16,12,21,
@@ -31,14 +29,13 @@ uint8_t awFmAsciiLetterToLetterIndex(char asciiLetter){
     21,15,21,21,21,21,21,21
   };
 
-  if(asciiLetter)
   if(asciiLetter == ('B' & charBitmask)){
     asciiLetter = (rand() & 1)? 'D'& charBitmask: 'N'& charBitmask;
   }
   else if(asciiLetter == ('Z' & charBitmask)){
     asciiLetter = (rand() & 1)? 'E'& charBitmask: 'Q'& charBitmask;
   }
-  else if(asciiLetter == 'X' & charBitmask){
+  else if(asciiLetter == ('X' & charBitmask)){
     //X is the ambiguity character for completely unknown, randomize the letter.
     asciiLetter = (rand() % 19) + 1;
     //if we randomized to another illegal character, add 1 to get a legal char.
@@ -63,15 +60,27 @@ uint8_t awFmAsciiLetterToLetterIndex(char asciiLetter){
  *    Value representing the compressed vector format of the amino acid.
  *
  */
-uint8_t awFmAsciiLetterToCompressedVectorFormat(const char asciiLetter){
+uint8_t awFmAsciiLetterToCompressedVectorFormat(const uint8_t asciiLetter){
   //bitmask to alias lowercase letter to uppercase letters
   const char charBitmask = 0x1F;
-  const letter_t compressedVectorLetters[32] = {
+  const uint8_t compressedVectorLetters[32] = {
     0x00, 0x1A, 0x00, 0x04, 0x06, 0x15, 0x1B, 0x16,
     0x02, 0x03, 0x00, 0x05, 0x1C, 0x01, 0x1E, 0x00,
     0x0C, 0x1D, 0x09, 0x13, 0x0A, 0x00, 0x19, 0x08,
     0x00, 0x17, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
   };
 
-  return compressedVectorLetters[asciiValue & charBitmask];
+  return compressedVectorLetters[asciiLetter & charBitmask];
+}
+
+
+uint8_t awFmCompressedVectorLetterToLetterIndex(const uint8_t compressedVectorLetter){
+  const uint8_t frequencyIndexLetters[32] = {
+    20, 16, 17,  6, 18,  7,  9, 20,
+    19,  8, 10, 20, 11, 20, 20, 20,
+    20, 20, 20,  5, 20,  4,  2, 15,
+    20,  3,  1, 14,  0, 13, 12, 20
+  };
+
+  return frequencyIndexLetters[compressedVectorLetter];
 }
--- AwFmLetter.h
+++ AwFmLetter.h
@@ -4,9 +4,11 @@
 #include <stdint.h>
 
 /*converts ASCII characters to frequency encoded values for query sequence*/
-uint8_t awFmAsciiLetterToLetterIndex(const char asciiLetter);
+uint8_t awFmAsciiLetterToLetterIndex(const uint8_t asciiLetter);
 
 /*converts ASCII characters to stridex, compressed vector format for database sequence*/
-uint8_t awFmAsciiLetterToCompressedVectorFormat(const char asciiLetter);
+uint8_t awFmAsciiLetterToCompressedVectorFormat(const uint8_t asciiLetter);
 
+/*converts a compressed vector format letter into a frequency encoded letter.*/
+uint8_t awFmCompressedVectorLetterToLetterIndex(const uint8_t compressedVectorLetter);
 #endif /* end of include guard: AW_FM_LETTER_H */

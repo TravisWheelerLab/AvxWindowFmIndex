9aa43265092214badad25fc7c4ae9dd0304313fc
Tim Anderson <tim.anderson@umontana.edu>
2019-09-26 18:02:04 -0600

    added short prototype descriptions

--- AwFmSearch.h
+++ AwFmSearch.h
@@ -6,16 +6,24 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/*Given the range of BWT positions where current kmer suffix is found, returns the
+    range where the suffix can be found after prepending the next letter.*/
 struct AwFmSearchRange awFmSearchKmerSuffix(const struct AwFmIndex *restrict const index,
   const struct AwFmSearchRange *restrict const currentRange, const uint8_t queryLetter);
 
+/*Returns an array of positions in the database sequence that are represented by the
+    given searchRange. The length of the returned array is equal to the difference
+    between the searchRange pointers. It is the caller's responsibility to free the
+    array that is returned.*/
 uint64_t *awFmFindDatabaseHitPositionsFromSearchRange(const struct AwFmIndex *restrict const index,
   const struct AwFmSearchRange *restrict const searchRange,
   enum AwFmFileAccessCode *restrict fileAccessResult);
 
+/*queries the AwFmIndex, returning a range of positions in the BWT where the given kmer is found.*/
 struct AwFmSearchRange awFmDatabaseSingleKmerExactMatch(const struct AwFmIndex *restrict const index,
-const char *restrict const kmer, const uint16_t kmerLength);
+  const char *restrict const kmer, const uint16_t kmerLength);
 
+/*queries the AwFmIndex, returning true if the given kmer is found in the database sequence.*/
 bool awFmSingleKmerExists(const struct AwFmIndex *restrict const index, const char *restrict const kmer,
   const uint16_t kmerLength);
 

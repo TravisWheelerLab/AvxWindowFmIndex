39c88b860eeb5f65eae96d7bb06064bc258fb59f
Tim Anderson <tim.anderson@umontana.edu>
2019-10-15 17:12:06 -0600

    move occ performance test to test directory

--- test/occupancyPerformanceTest/occSpeedTest.c
+++ test/occupancyPerformanceTest/occSpeedTest.c
@@ -4,9 +4,9 @@
 #include <stdbool.h>
 #include <time.h>
 
-#include "../AwFmIndex.h"
-#include "../AwFmOccupancy.h"
-#include "../AwFmGlobals.h"
+#include "../../AwFmIndex.h"
+#include "../../AwFmOccupancy.h"
+#include "../../AwFmGlobals.h"
 
 
 // #define AVX_VECTOR_PREFETCH

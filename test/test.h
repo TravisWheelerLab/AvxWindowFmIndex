47616fb05c2ca679fcd04a827df7b93697013a9b
Tim Anderson <tim.anderson@umontana.edu>
2019-10-28 17:24:08 -0600

    change test failure strings to be colored red

--- test/test.h
+++ test/test.h
@@ -9,20 +9,25 @@
 
 #define testAssert(assertion) (awFmAssertTest(assertion, __FILE__, __func__, __LINE__))
 #define testAssertString(assertion, message) (awFmAssertTestString(assertion, message, __FILE__, __func__, __LINE__))
-
+void setPrintfColorRed(){      printf("\033[1;31m");}
+void setPrintfColorDefault(){  printf("\033[0m");}
 
 size_t assertionNumber = 0;
-inline void awFmAssertTest(bool assertion, const char *file, const char *func, const int line){
+void awFmAssertTest(bool assertion, const char *file, const char *func, const int line){
   if(!assertion){
+    setPrintfColorRed();
     printf("assertion failure at test %zu, file %s, func %s, line %d\n", assertionNumber, file, func, line);
+    setPrintfColorDefault();
   }
   assertionNumber++;
 }
 
 
-inline void awFmAssertTestString(bool assertion, char *message, const char *file, const char *func, const int line){
+void awFmAssertTestString(bool assertion, char *message, const char *file, const char *func, const int line){
   if(!assertion){
+    setPrintfColorRed();
     printf("assertion failure at test %zu, file %s, func %s, line %d\t %s\n", assertionNumber, file, func, line, message);
+    setPrintfColorDefault();
   }
   assertionNumber++;
 }

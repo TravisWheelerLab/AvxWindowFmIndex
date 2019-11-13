6bdb130991528c4d140e1af72130803c1a4e2d53
Tim Anderson <tim.anderson@umontana.edu>
2019-11-12 18:07:32 -0700

    split search functions into forward and backward search.

--- AwFmSearch.c
+++ AwFmSearch.c
@@ -3,11 +3,11 @@
 #include "AwFmSearch.h"
 
 /*
- * Function:  AwFmIterativeRangeBackwardSearch
+ * Function:  AwFmIterativeAminoAcidRangeBackwardSearch
  * --------------------
  *  Queries the FM-Index to iteratively prepend an additional letter to the
- *    implicit kmer suffix, and return a new range of positions in the BWT where
- *    the extended kmer suffix may be found.
+ *    implicit kmer suffix, and return a new range of positions in the amino acid
+ *    BWT where the extended kmer suffix may be found.
  *
  *  If the given kmer is not found, the AwFmSearch Range will result in a false value
  *   when given to the awFmSearchRangeIsValid function.
@@ -22,61 +22,38 @@
  *    kmer suffixmay be found, as long as startPtr < endPtr. Otherwise (startPtr >= endPtr),
  *    the given kmer suffix does not exist in the database sequence.
  */
-struct AwFmSearchRange AwFmIterativeRangeBackwardSearch(const struct AwFmIndex *restrict const index,
-  const struct AwFmSearchRange *restrict const currentRange, const uint8_t prefixLetter){
+
+struct AwFmSearchRange AwFmIterativeAminoAcidForwardSearch(const struct AwFmIndex *restrict const index, const struct AwFmSearchRange *restrict const currentRange,
+  const uint8_t prefixLetter){
+  //TODO: implement this function
+}
+
+
+ struct AwFmSearchRange AwFmIterativeAminoAcidBackwardSearch(const struct AwFmIndex *restrict const index, const struct AwFmSearchRange *restrict const currentRange,
+   const uint8_t prefixLetter){
 
   struct AwFmSearchRange range;
   const uint64_t letterRankPrefixSum = index->rankPrefixSums[prefixLetter];
+  const struct AwFmAminoBlock *restrict const blockList = index->backwardBwtBlockList.asAmino;
 
-  range.startPtr = awFmGetOccurances(index, currentRange->startPtr, prefixLetter) + letterRankPrefixSum;
-  awFmOccurancesDataPrefetch(index, range.startPtr);
+  range.startPtr = awFmGetAminoAcidOccurrence(blockList, currentRange->startPtr - 1, prefixLetter) + letterRankPrefixSum;
+  awFmAminoAcidDataPrefetch(blockList, range.startPtr);
 
-  range.endPtr = awFmGetOccurances(index, currentRange->endPtr, prefixLetter) + letterRankPrefixSum;
-  awFmOccurancesDataPrefetch(index, range.endPtr);
+  range.endPtr = awFmGetAminoAcidOccurrence(blockList, currentRange->endPtr, prefixLetter) - 1 + letterRankPrefixSum;
+  awFmAminoAcidDataPrefetch(blockList, range.endPtr);
 
   return range;
 }
 
-//TODO: check this functionality against the literature.
-/*
- * Function:  AwFmIterativeRangeForwardSearch
- * --------------------
- *  Queries the FM-Index to iteratively append an additional letter to the
- *    implicit kmer suffix, and return a new range of positions in the BWT where
- *    the extended kmer suffix may be found.
- *
- *  If the given kmer is not found, the AwFmSearch Range will result in a false value
- *   when given to the awFmSearchRangeIsValid function.
- *
- *  Inputs:
- *    index:          Pointer to the valid AwFmIndex struct.
- *    currentRange:   Range of positions in the BWT where the current kmer suffix may be found.
- *    suffixLetter:   Letter that will be appended to the implicit kmer to extend the kmer suffix.
- *
- *  Returns:
- *    AwFmSearch range representing the range of BWT positions where the implicit
- *    kmer suffixmay be found, as long as startPtr < endPtr. Otherwise (startPtr >= endPtr),
- *    the given kmer suffix does not exist in the database sequence.
- */
-struct AwFmSearchRange AwFmIterativeRangeForwardSearch(const struct AwFmIndex *restrict const index,
-  const struct AwFmSearchRange *restrict const currentRange, const uint8_t suffixLetter){
-
-      struct AwFmSearchRange range;
-      const uint64_t letterRankPrefixSum = index->rankPrefixSums[suffixLetter];
-
-      range.startPtr = awFmGetOccurances(index, currentRange->startPtr, suffixLetter) + letterRankPrefixSum;
-      awFmOccurancesDataPrefetch(index, range.startPtr);
-
-      range.endPtr = awFmGetOccurances(index, currentRange->endPtr, suffixLetter) + letterRankPrefixSum;
-      awFmOccurancesDataPrefetch(index, range.endPtr);
-
-      return range;
+void AwFmIterativeAminoAcidBiDirectionalSearch(const struct AwFmIndex *restrict const index,
+  struct AwFmSearchRange *restrict const currentForwardRange, struct AwFmSearchRange *restrict const currentBackwardRange,
+  const uint8_t suffixLetter){
+  //todo: implement this
 }
 
 
-
 /*
- * Function:  awFmFindDatabaseHitPositionsFromSearchRange
+ * Function:  awFmFindAminoAcidDatabaseHitPositionsForward
  * --------------------
  *  Takes a range of BWT positions, backtraces each position to find the nearest sample in the
  *    compressed suffix array, and looks up those suffix array positions on disk to
@@ -108,7 +85,7 @@ struct AwFmSearchRange AwFmIterativeRangeForwardSearch(const struct AwFmIndex *r
  *      AwFmIllegalPositionError on a suffix array position being out of bounds of
  *        the file's compressed suffix array.
  */
-uint64_t *awFmFindDatabaseHitPositionsFromSearchRange(const struct AwFmIndex *restrict const index,
+uint64_t *awFmFindAminoAcidDatabaseHitPositionsForward(const struct AwFmIndex *restrict const index,
   const struct AwFmSearchRange *restrict const searchRange,
   enum AwFmReturnCode *restrict fileAccessResult){
 
@@ -126,7 +103,7 @@ uint64_t *awFmFindDatabaseHitPositionsFromSearchRange(const struct AwFmIndex *re
 
   //call a prefetch for each block that contains the positions that we need to start querying
   for(uint64_t i = searchRange->startPtr; i < searchRange->endPtr; i += POSITIONS_PER_FM_BLOCK){
-    awFmOccurancesDataPrefetch(index, i);
+    awFmAminoAcidDataPrefetch(index->forwardBwtBlockLists.asAmino, i);
   }
 
   //backtrace each position until we have a list of the positions in the database sequence.
@@ -152,6 +129,81 @@ uint64_t *awFmFindDatabaseHitPositionsFromSearchRange(const struct AwFmIndex *re
 
 
 /*
+ * Function:  awFmFindAminoAcidDatabaseHitPositionsBackward
+ * --------------------
+ *  Takes a range of BWT positions, backtraces each position to find the nearest sample in the
+ *    compressed suffix array, and looks up those suffix array positions on disk to
+ *    determine the corresponding database sequence position for each BWT position
+ *    between the searchRange's pointers (inclusive startPtr, exclusive endPtr).
+ *
+ *  It is the caller's responsibility to free() the returned sequence position array.
+ *
+ *  This function will overwrite the data in the positionArray, returning the
+ *    database sequence positions in the corresponding elements of the positionArray.
+ *
+ *  Note: positionArray and offsetArray should be of equal length. Having either shorter
+ *    than positionArrayLength will result in undefined behavior.
+ *
+ *  Inputs:
+ *    index:              Pointer to the valid AwFmIndex struct.
+ *    positionArray:          Array of positions in the implied full suffix array to load.
+ *      This function will convert these positions to indices in the compressed suffix array,
+ *      as long as the positions are multiples of the suffix array compression ratio.
+ *    offsetArray:  array of offsets to be added to the database sequence positions.
+ *      This is needed because we can only query the suffix array on positions that are sampled.
+ *    positionArrayLength:    Length of the positionArray and offsetArray.
+ *
+ *  Returns:
+ *    AwFmReturnCode detailing the result of the read attempt. Possible return values:
+ *      AwFmFileReadOkay on success,
+ *      AwFmFileOpenFail on failure to open the AwFm Index file
+ *      AwFmFileReadFail on failure to read as many characters as was expected by the sequence.
+ *      AwFmIllegalPositionError on a suffix array position being out of bounds of
+ *        the file's compressed suffix array.
+ */
+uint64_t *awFmFindAminoAcidDatabaseHitPositionsBackward(const struct AwFmIndex *restrict const index,
+  const struct AwFmSearchRange *restrict const searchRange,
+  enum AwFmReturnCode *restrict fileAccessResult){
+
+  const uint64_t numPositionsInRange            = awFmSearchRangeLength(searchRange);
+
+  //allocate the position array and offsetArray (uses one malloc call, only needs 1 free call)
+  //restrict keywords should be okay, since they shouldn't read/write to overlapping elements
+  uint64_t *const restrict positionArray  = malloc(numPositionsInRange * sizeof(uint64_t) * 2);
+  uint64_t *const restrict offsetArray    = positionArray + numPositionsInRange;
+  //check for allocation failures
+  if(__builtin_expect(positionArray == NULL, 0)){
+    *fileAccessResult = AwFmAllocationFailure;
+    return NULL;
+  }
+
+  //call a prefetch for each block that contains the positions that we need to start querying
+  for(uint64_t i = searchRange->startPtr; i < searchRange->endPtr; i += POSITIONS_PER_FM_BLOCK){
+    awFmAminoAcidDataPrefetch(index->backwardBwtBlockList.asAmino, i);
+  }
+
+  //backtrace each position until we have a list of the positions in the database sequence.
+  for(uint64_t i=0; i < numPositionsInRange; i++){
+    uint64_t databaseSequenceOffset = 0;
+    uint64_t backtracePosition      = searchRange->startPtr + i;
+
+    while(!awFmBwtPositionIsSampled(index, backtracePosition)){
+      backtracePosition = awFmBackstepBwtPosition(index, backtracePosition);
+      databaseSequenceOffset++;
+    }
+
+    positionArray[i]  = backtracePosition;
+    offsetArray[i]    = databaseSequenceOffset;
+  }
+
+  *fileAccessResult = awFmDbSequencePositionsFromSuffixArrayFile(index, positionArray,
+    offsetArray, numPositionsInRange);
+
+  return positionArray;
+}
+
+
+/*
  * Function:  awFmDatabaseSingleKmerExactMatch
  * --------------------
  *  Queries the FM-Index for the range of BWT positions that represent instances
@@ -173,23 +225,45 @@ uint64_t *awFmFindDatabaseHitPositionsFromSearchRange(const struct AwFmIndex *re
  *    kmer may be found, as long as startPtr < endPtr. Otherwise (startPtr >= endPtr),
  *    the given kmer does not exist in the database sequence.
  */
-struct AwFmSearchRange awFmDatabaseSingleKmerExactMatch(const struct AwFmIndex *restrict const index,
+struct AwFmSearchRange awFmDatabaseSingleAminoAcidKmerExactMatch(const struct AwFmIndex *restrict const index,
 const char *restrict const kmer, const uint16_t kmerLength){
-  uint_fast16_t kmerQueryPosition = kmerLength - 1;
 
-  //set the initial range from the rankPrefixSums
-  const uint8_t firstSuffixLetter = kmer[kmerQueryPosition];
-  struct AwFmSearchRange searchRange = {index->rankPrefixSums[firstSuffixLetter], index->rankPrefixSums[firstSuffixLetter + 1]};
 
-  //request the data be prefetched for each occurance call.
-  awFmOccurancesDataPrefetch(index, searchRange.startPtr);
-  awFmOccurancesDataPrefetch(index, searchRange.endPtr);
-  do{
-    kmerQueryPosition--;
-    searchRange = AwFmIterativeRangeBackwardSearch(index, &searchRange, kmer[kmerQueryPosition]);
-  }while(__builtin_expect(awFmSearchRangeIsValid(&searchRange) && kmerQueryPosition, 1));
+  if(index->metadata.bwtType  == AwFmBwtTypeBackwardOnly){
+    uint_fast16_t kmerQueryPosition = kmerLength - 1;
 
-  return searchRange;
+    //set the initial range from the rankPrefixSums
+    const uint8_t firstSuffixLetter = kmer[kmerQueryPosition];
+    struct AwFmSearchRange searchRange = {index->rankPrefixSums[firstSuffixLetter], index->rankPrefixSums[firstSuffixLetter + 1]};
+
+    //request the data be prefetched for each occurance call.
+    awFmAminoAcidDataPrefetch(index->backwardBwtBlockList.asAmino, searchRange.startPtr);
+    awFmAminoAcidDataPrefetch(index->backwardBwtBlockList.asAmino, searchRange.endPtr);
+
+    while(__builtin_expect(awFmSearchRangeIsValid(&searchRange) && kmerQueryPosition, 1)){
+      kmerQueryPosition--;
+      searchRange = AwFmIterativeAminoAcidForwardSearch(index, &searchRange, kmer[kmerQueryPosition]);
+    }
+
+    return searchRange;
+  }
+  else{
+    uint_fast16_t kmerQueryPosition = 0;
+
+    //set the initial range from the rankPrefixSums
+    const uint8_t firstPrefixLetter = kmer[0];
+    struct AwFmSearchRange searchRange = {index->rankPrefixSums[firstPrefixLetter], index->rankPrefixSums[firstPrefixLetter + 1]};
+
+    //request the data be prefetched for each occurance call.
+    awFmAminoAcidDataPrefetch(index->forwardBwtBlockLists.asAmino, searchRange.startPtr);
+    awFmAminoAcidDataPrefetch(index->forwardBwtBlockLists.asAmino, searchRange.endPtr);
+    while(__builtin_expect(awFmSearchRangeIsValid(&searchRange) && kmerQueryPosition, 1)){
+      kmerQueryPosition++;
+      searchRange = AwFmIterativeAminoAcidForwardSearch(index, &searchRange, kmer[kmerQueryPosition]);
+    }
+
+    return searchRange;
+  }
 }
 
 
--- AwFmSearch.h
+++ AwFmSearch.h
@@ -12,8 +12,9 @@
 
 /*Given the range of BWT positions where current kmer suffix is found, returns the
     range where the suffix can be found after prepending the next prefix letter.*/
-struct AwFmSearchRange AwFmIterativeRangeBackwardSearch(const struct AwFmIndex *restrict const index,
-  const struct AwFmSearchRange *restrict const currentRange, const uint8_t prefixLetter);
+struct AwFmSearchRange AwFmIterativeAminoAcidRangeBackwardSearch(const struct AwFmAminoBlock *restrict const blockList,
+  const uint64_t *restrict const rankPrefixSums, const struct AwFmSearchRange *restrict const currentRange,
+  const uint8_t prefixLetter);
 
 /*Given the range of BWT positions where current kmer suffix is found, returns the
     range where the suffix can be found after appending the next suffix letter.*/
@@ -24,7 +25,15 @@ struct AwFmSearchRange AwFmIterativeRangeForwardSearch(const struct AwFmIndex *r
     given searchRange. The length of the returned array is equal to the difference
     between the searchRange pointers. It is the caller's responsibility to free the
     array that is returned.*/
-uint64_t *awFmFindDatabaseHitPositionsFromSearchRange(const struct AwFmIndex *restrict const index,
+uint64_t *awFmFindAminoAcidDatabaseHitPositionsForward(const struct AwFmIndex *restrict const index,
+  const struct AwFmSearchRange *restrict const searchRange,
+  enum AwFmReturnCode *restrict fileAccessResult);
+
+/*Returns an array of positions in the database sequence that are represented by the
+    given searchRange. The length of the returned array is equal to the difference
+    between the searchRange pointers. It is the caller's responsibility to free the
+    array that is returned.*/
+uint64_t *awFmFindAminoAcidDatabaseHitPositionsBackward(const struct AwFmIndex *restrict const index,
   const struct AwFmSearchRange *restrict const searchRange,
   enum AwFmReturnCode *restrict fileAccessResult);
 

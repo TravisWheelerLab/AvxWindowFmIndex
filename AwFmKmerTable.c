33b445b8902c515385fe2deda0e14977f5f18afe
Tim Anderson <tim.anderson@umontana.edu>
2020-01-02 15:34:09 -0700

    add partial kmer lookup, change to return struct rather than pointer
    While returning a pointer to the data in the lookup table is great for 
    full kmers, this approach doesn't work for partial kmers, since it needs 
    to assemble a new range.

--- AwFmKmerTable.c
+++ AwFmKmerTable.c
@@ -1,18 +1,42 @@
 #include "AwFmKmerTable.h"
 #include "AwFmLetter.h"
+#include <stdlib.h>
+#include <string.h>
 
- struct AwFmBackwardRange *awFmSeedKmerRangeFromTable(const struct AwFmIndex *restrict const index,
-   const char *restrict const kmer){
 
-   const uint8_t kmerSeedLength = index->metadata.kmerLengthInSeedTable;
-   size_t tableIndex = 0;
+struct AwFmBackwardRange awFmSeedKmerRangeFromTable(const struct AwFmIndex *restrict const index,
+  const char *restrict const kmer, const uint8_t kmerLength){
 
-   for(int_fast16_t i = kmerSeedLength - 1; i >= 0; i--){
-     if(index->metadata.alphabetType == AwFmAlphabetNucleotide)
-       tableIndex = (tableIndex * AW_FM_NUCLEOTIDE_CARDINALITY) + awFmNucleotideLetterIndexToAscii(kmer[i]);
-     else
-       tableIndex = (tableIndex * AW_FM_AMINO_CARDINALITY) + awFmAminoAcidLetterIndexToAscii(kmer[i]);
-   }
+  if(kmerLength < index->metadata.kmerLengthInSeedTable){
+    char extendedKmerBuffer[index->metadata.kmerLengthInSeedTable];
+    memcpy(extendedKmerBuffer, kmer, kmerLength);
+    memset(extendedKmerBuffer + kmerLength, 'a', index->metadata.kmerLengthInSeedTable - kmerLength);
+    const uint64_t lowerRange = awFmSeedKmerRangeFromTableExactLength(index, extendedKmerBuffer).startPtr;
 
-   return &(index->kmerSeedTable[tableIndex]);
- }
+    const char lastChar = index->metadata.alphabetType == AwFmAlphabetNucleotide? 't': 'y';
+    memset(extendedKmerBuffer + kmerLength, lastChar, index->metadata.kmerLengthInSeedTable - kmerLength);
+    const uint64_t upperRange = awFmSeedKmerRangeFromTableExactLength(index, extendedKmerBuffer).startPtr;
+
+    return (struct AwFmBackwardRange){lowerRange, upperRange};
+  }
+  else{
+    return awFmSeedKmerRangeFromTableExactLength(index, kmer);
+  }
+}
+
+
+struct AwFmBackwardRange awFmSeedKmerRangeFromTableExactLength(const struct AwFmIndex *restrict const index,
+  const char *restrict const kmer){
+
+  const uint8_t kmerSeedLength = index->metadata.kmerLengthInSeedTable;
+  size_t tableIndex = 0;
+
+  for(int_fast16_t i = kmerSeedLength - 1; i >= 0; i--){
+    if(index->metadata.alphabetType == AwFmAlphabetNucleotide)
+      tableIndex = (tableIndex * AW_FM_NUCLEOTIDE_CARDINALITY) + awFmNucleotideLetterIndexToAscii(kmer[i]);
+    else
+      tableIndex = (tableIndex * AW_FM_AMINO_CARDINALITY) + awFmAminoAcidLetterIndexToAscii(kmer[i]);
+  }
+
+  return (index->kmerSeedTable[tableIndex]);
+}
--- AwFmKmerTable.h
+++ AwFmKmerTable.h
@@ -1,10 +1,30 @@
 #include <stdint.h>
 #include "AwFmIndex.h"
 
+
 /*
  * Function:  awFmSeedKmerRangeFromTable
  * --------------------
  * Given a kmer, queries the kmerSeedTable  for the partially completed range in the backward suffix array.
+ * This function checks the kmerLength, and if the kmer is smaller than those memoized in the seed table,
+ * it extends the kmer to find the range the kmer comprises.
+ *
+ *  Inputs:
+ *    index: AwFmIndex struct to search
+ *    kmer: character string to search for in the kmerSeedTable.
+ *    kmerLength: length of the given kmer
+ *
+ *  Returns:
+ *    struct containing the startPtr and endPtr for the kmer.
+ */
+struct AwFmBackwardRange awFmSeedKmerRangeFromTable(const struct AwFmIndex *restrict const index,
+  const char *restrict const kmer, const uint8_t kmerLength);
+
+
+/*
+ * Function:  awFmSeedKmerRangeFromTableExactLength
+ * --------------------
+ * Given a kmer, queries the kmerSeedTable for the partially completed range in the backward suffix array.
  * The length of the kmer's stored in the seed table can be found in the index metadata's kmerLengthInSeedTable attribute.
  * The given kmer must be at least this long.
  *
@@ -13,7 +33,7 @@
  *    kmer: character string to search for in the kmerSeedTable.
  *
  *  Returns:
- *    pointer to the backwardsRange struct in the kmerSeedTable.
+ *    struct containing the backwardsRange struct in the kmerSeedTable.
  */
-struct AwFmBackwardRange *awFmSeedKmerRangeFromTable(const struct AwFmIndex *restrict const index,
+struct AwFmBackwardRange awFmSeedKmerRangeFromTableExactLength(const struct AwFmIndex *restrict const index,
   const char *restrict const kmer);

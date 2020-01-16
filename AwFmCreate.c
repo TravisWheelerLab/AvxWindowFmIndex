a9554d7853bd391dbe9c20cbc1b53c8628e8b1c0
Tim Anderson <tim.anderson@umontana.edu>
2020-01-16 16:36:44 -0700

    rework BWT creation to be cleaner, more correct, copy over prefix sums

--- AwFmCreate.c
+++ AwFmCreate.c
@@ -40,40 +40,26 @@ enum AwFmReturnCode awFmCreateIndex(struct AwFmIndex *restrict *index,
   indexData->bwtLength = sequenceLength + 1;
 
   uint64_t *suffixArray = malloc((sequenceLength + 1) * sizeof(uint64_t));
+  //hard code in the sentinel character, since it will always be in the first element, we know
+  //the value, and libdivsufsort doesn't add the sentinel to the beginning when it creates
+  //the suffix array.
+  suffixArray[0] = sequenceLength;
+
   if(suffixArray == NULL){
     awFmDeallocIndex(indexData);
     return AwFmAllocationFailure;
   }
-  uint64_t divSufSortReturnCode = divsufsort64(sequence, (int64_t*)suffixArray, sequenceLength);
+  //create the suffix array, storing it starting in the second element of the suffix array we allocated.
+  //this doesn't clobber the sentinel we added earlier, and makes for easier bwt creation.
+  uint64_t divSufSortReturnCode = divsufsort64(sequence, (int64_t*)(suffixArray + 1), sequenceLength);
   if(divSufSortReturnCode < 0){
     free(suffixArray);
     awFmDeallocIndex(indexData);
     return AwFmSuffixArrayCreationFailure;
   }
 
-  printf("sequence: ");
-  for(uint64_t i = 0; i < sequenceLength; i++){
-    if(sequence)
-    printf("%c, ", sequence[i]);
-  }
-
-  printf("\n");
-  printf("suffix array: ");
-  for(uint64_t i = 0; i < sequenceLength; i++){
-    printf("%zu, ", suffixArray[i]);
-  }
-  printf("\n");
-
-
-  printf("bwt: ");
-  for(uint64_t i = 0; i < sequenceLength; i++){
-    if(sequence)
-    printf("%c, ", sequence[suffixArray[i]-1]);
-  }
-  printf("\n");
-
   //set the bwt and prefix sums
-  setBwtAndPrefixSums(indexData, sequenceLength + 1, sequence, suffixArray);
+  setBwtAndPrefixSums(indexData, indexData->bwtLength, sequence, suffixArray);
 
   struct AwFmSearchRange searchRange;
   populateKmerSeedTable(indexData, searchRange, 0, 0);
@@ -86,10 +72,11 @@ enum AwFmReturnCode awFmCreateIndex(struct AwFmIndex *restrict *index,
   indexData->sequenceFileOffset    = awFmGetSequenceFileOffset(*index);
   //file descriptor will be set in awFmWriteIndexToFile
 
-  //create the file and return
+  //create the file
   enum AwFmReturnCode returnCode = awFmWriteIndexToFile(indexData, suffixArray, sequence, sequenceLength,
     fileSrc, allowFileOverwrite);
 
+  //since the suffix array is allocated in this function, we need to clean it up before returning.
   free(suffixArray);
 
   return returnCode;
@@ -97,12 +84,12 @@ enum AwFmReturnCode awFmCreateIndex(struct AwFmIndex *restrict *index,
 
 
 
-void setBwtAndPrefixSums(struct AwFmIndex *restrict const index, const size_t sequenceLength,
+void setBwtAndPrefixSums(struct AwFmIndex *restrict const index, const size_t bwtLength,
   const uint8_t *restrict const sequence, const uint64_t *restrict const suffixArray){
   if(index->metadata.alphabetType == AwFmAlphabetNucleotide){
     uint64_t baseOccurrences[4] = {0};
 
-    for(uint64_t suffixArrayPosition = 0; suffixArrayPosition < sequenceLength; suffixArrayPosition++){
+    for(uint64_t suffixArrayPosition = 0; suffixArrayPosition < bwtLength; suffixArrayPosition++){
       const size_t  blockIndex      = suffixArrayPosition / AW_FM_POSITIONS_PER_FM_BLOCK;
       const uint8_t positionInBlock = suffixArrayPosition % AW_FM_POSITIONS_PER_FM_BLOCK;
       const uint8_t byteInVector    = positionInBlock / 8;
@@ -110,24 +97,18 @@ void setBwtAndPrefixSums(struct AwFmIndex *restrict const index, const size_t se
       struct AwFmNucleotideBlock *nucleotideBlockPtr = &index->bwtBlockList.asNucleotide[blockIndex];
       uint8_t *restrict const letterBitVectorBytes = (uint8_t*)nucleotideBlockPtr->letterBitVectors;
 
-      if(positionInBlock == 0){
+      if(__builtin_expect(positionInBlock == 0, 0)){
         //when we start a new block, copy over the base occurrences, and initialize the bit vectors
         memcpy(nucleotideBlockPtr->baseOccurrences, baseOccurrences, AW_FM_NUCLEOTIDE_CARDINALITY * sizeof(uint64_t));
         memset(nucleotideBlockPtr->letterBitVectors, 0, sizeof(__m256i) * AW_FM_NUCLEOTIDE_VECTORS_PER_WINDOW);
       }
 
-      uint64_t sequencePosition;
-      if(__builtin_expect(suffixArrayPosition == 0, 0)){
-        //the first position in the bwt stores the char before the sentinel, aka the last letter in the sequence.
-        sequencePosition = sequenceLength - 1;
-      }
-      else{
-        sequencePosition = suffixArray[suffixArrayPosition] - 1;
-        if(__builtin_expect(sequencePosition == 0, 0)){
-          //sentinel character at this position, don't shift anything in
-          continue;
-        }
-        uint8_t letterIndex = awFmAsciiNucleotideToLetterIndex(sequence[suffixArrayPosition]);
+      uint64_t sequencePositionInSuffixArray = suffixArray[suffixArrayPosition];
+      if(__builtin_expect(sequencePositionInSuffixArray != 0, 1)){
+        // printf("\tsequence position %zu, ", sequencePositionInSuffixArray);
+        // printf("bwt letter: %c.\n", sequence[sequencePositionInSuffixArray - 1]);
+        uint64_t positionInBwt = sequencePositionInSuffixArray - 1;
+        uint8_t letterIndex = awFmAsciiNucleotideToLetterIndex(sequence[positionInBwt]);
         letterBitVectorBytes[byteInVector]      = letterBitVectorBytes[byteInVector] | (((letterIndex >> 0) & 0x1) << bitInVectorByte);
         letterBitVectorBytes[byteInVector + 32] = letterBitVectorBytes[byteInVector] | (((letterIndex >> 1) & 0x1) << bitInVectorByte);
       }
@@ -141,11 +122,12 @@ void setBwtAndPrefixSums(struct AwFmIndex *restrict const index, const size_t se
       baseOccurrences[i] = prevBaseOccurrence;
       prevBaseOccurrence += tempBaseOccurrence;
     }
+    memcpy(index->prefixSums, baseOccurrences, AW_FM_NUCLEOTIDE_CARDINALITY * sizeof(uint64_t));
   }
   else{
     uint64_t baseOccurrences[20] = {0};
 
-    for(uint64_t suffixArrayPosition = 0; suffixArrayPosition < sequenceLength; suffixArrayPosition++){
+    for(uint64_t suffixArrayPosition = 0; suffixArrayPosition < bwtLength; suffixArrayPosition++){
       const size_t  blockIndex      = suffixArrayPosition / AW_FM_POSITIONS_PER_FM_BLOCK;
       const uint8_t positionInBlock = suffixArrayPosition % AW_FM_POSITIONS_PER_FM_BLOCK;
       const uint8_t byteInVector    = positionInBlock / 8;
@@ -153,30 +135,24 @@ void setBwtAndPrefixSums(struct AwFmIndex *restrict const index, const size_t se
       struct AwFmAminoBlock *restrict const aminoBlockPointer = &index->bwtBlockList.asAmino[blockIndex];
       uint8_t *restrict const letterBitVectorBytes = (uint8_t*)aminoBlockPointer->letterBitVectors;
 
-      if(positionInBlock == 0){
+      if(__builtin_expect(positionInBlock == 0, 0)){
         //when we start a new block, copy over the base occurrences, and initialize the bit vectors
         memcpy(aminoBlockPointer->baseOccurrences, baseOccurrences, AW_FM_AMINO_CARDINALITY * sizeof(uint64_t));
         memset(aminoBlockPointer->letterBitVectors, 0, sizeof(__m256i) * AW_FM_AMINO_VECTORS_PER_WINDOW);
       }
 
-      uint64_t sequencePosition;
-      if(__builtin_expect(suffixArrayPosition == 0, 0)){
-        //the first position in the bwt stores the char before the sentinel, aka the last letter in the sequence.
-         sequencePosition = sequenceLength-1;
+      uint64_t sequencePositionInSuffixArray = suffixArray[suffixArrayPosition];
+      if(__builtin_expect(sequencePositionInSuffixArray != 0, 1)){
+        // printf("\tsequence position %zu, ", sequencePositionInSuffixArray);
+        // printf("bwt letter: %c.\n", sequence[sequencePositionInSuffixArray - 1]);
+        uint64_t positionInBwt = sequencePositionInSuffixArray - 1;
+        uint8_t letterIndex = awFmAsciiAminoAcidToLetterIndex(sequence[positionInBwt]);
+        letterBitVectorBytes[byteInVector]        = letterBitVectorBytes[byteInVector] | (((letterIndex >> 0) & 0x1) << bitInVectorByte);
+        letterBitVectorBytes[byteInVector + 32]   = letterBitVectorBytes[byteInVector] | (((letterIndex >> 1) & 0x1) << bitInVectorByte);
+        letterBitVectorBytes[byteInVector + 64]   = letterBitVectorBytes[byteInVector] | (((letterIndex >> 2) & 0x1) << bitInVectorByte);
+        letterBitVectorBytes[byteInVector + 96]   = letterBitVectorBytes[byteInVector] | (((letterIndex >> 3) & 0x1) << bitInVectorByte);
+        letterBitVectorBytes[byteInVector + 128]  = letterBitVectorBytes[byteInVector] | (((letterIndex >> 4) & 0x1) << bitInVectorByte);
       }
-      else{
-        uint64_t sequencePosition = suffixArray[suffixArrayPosition] - 1;
-        if(__builtin_expect(sequencePosition == 0, 0)){
-          //sentinel character at this position
-          continue;
-        }
-      }
-      uint8_t letterIndex = awFmAsciiAminoAcidToLetterIndex(sequence[sequencePosition]);
-      letterBitVectorBytes[byteInVector]        = letterBitVectorBytes[byteInVector] | (((letterIndex >> 0) & 0x1) << bitInVectorByte);
-      letterBitVectorBytes[byteInVector + 32]   = letterBitVectorBytes[byteInVector] | (((letterIndex >> 1) & 0x1) << bitInVectorByte);
-      letterBitVectorBytes[byteInVector + 64]   = letterBitVectorBytes[byteInVector] | (((letterIndex >> 2) & 0x1) << bitInVectorByte);
-      letterBitVectorBytes[byteInVector + 96]   = letterBitVectorBytes[byteInVector] | (((letterIndex >> 3) & 0x1) << bitInVectorByte);
-      letterBitVectorBytes[byteInVector + 128]  = letterBitVectorBytes[byteInVector] | (((letterIndex >> 4) & 0x1) << bitInVectorByte);
     }
 
     //set the prefix sums
@@ -187,6 +163,7 @@ void setBwtAndPrefixSums(struct AwFmIndex *restrict const index, const size_t se
       baseOccurrences[i] = prevBaseOccurrence;
       prevBaseOccurrence += tempBaseOccurrence;
     }
+    memcpy(index->prefixSums, baseOccurrences, AW_FM_AMINO_CARDINALITY * sizeof(uint64_t));
   }
 }
 
@@ -201,7 +178,7 @@ void populateKmerSeedTable(struct AwFmIndex *restrict const index, struct AwFmSe
 
   //base case
   if(kmerLength == currentKmerLength){
-    // printf("memoizing kmer range %zu-%zu\n", searchRange.startPtr, searchRange.endPtr);
+    // printf("memoizing kmer range %zu-%zu into index %zu\n", searchRange.startPtr, searchRange.endPtr, currentKmerIndex);
     memcpy(&index->kmerSeedTable[currentKmerIndex], &searchRange, sizeof(struct AwFmSearchRange));
     return;
   }

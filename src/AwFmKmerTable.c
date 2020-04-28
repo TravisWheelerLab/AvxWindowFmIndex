32264bfbd65a8c392dcb714ae04d349c680de5c9
Tim Anderson <tim.anderson@umontana.edu>
2020-04-28 14:28:03 -0600

    updated kmer table to no longer look for kmers < those in the kmer table
    This change is due to the change where ambiguity codes become sentinel characters. The previous technique of stitching together ranges is not mathematically valid for the new approach.

--- src/AwFmKmerTable.c
+++ src/AwFmKmerTable.c
@@ -2,81 +2,28 @@
 #include "AwFmLetter.h"
 
 
-//TODO: uncomment this function prototype when it is removed from the header
-// uint8_t kmerMatchesInSequenceEnding(const struct AwFmIndex *restrict const index,
-//   const uint64_t kmerIndexEncoding, const uint8_t kmerLength);
-
-
 struct AwFmSearchRange awFmNucleotideKmerSeedRangeFromTable(const struct AwFmIndex *restrict const index,
   const char *restrict const kmer, const uint8_t kmerLength){
 
-  const bool kmerIsShorterThanTable   = kmerLength < index->metadata.kmerLengthInSeedTable;
-  const uint8_t kmerSeedStartPosition = kmerIsShorterThanTable? 0: kmerLength - index->metadata.kmerLengthInSeedTable;
-
+  const uint8_t kmerSeedStartPosition = kmerLength - index->metadata.kmerLengthInSeedTable;
   uint64_t kmerTableIndex = 0;
   for(int_fast16_t i = kmerSeedStartPosition; i < kmerLength; i++){
     uint8_t letterIndex = awFmAsciiNucleotideToLetterIndex(kmer[i]);
     kmerTableIndex = (kmerTableIndex * AW_FM_NUCLEOTIDE_CARDINALITY) + letterIndex;
   }
 
-  if(kmerIsShorterThanTable){
-    uint8_t numImplicitAppendedLetters  = index->metadata.kmerLengthInSeedTable - kmerLength;
-    uint64_t kmerRangeStartIndex        = kmerTableIndex << (2* numImplicitAppendedLetters);
-    uint64_t kmerRangeEndIndex          = ((kmerTableIndex+1) << (2 * numImplicitAppendedLetters)) - 1;
-    uint8_t numMatchesInSequenceEnding  = kmerMatchesInSequenceEnding(index, kmerRangeStartIndex, kmerLength);
-
-    return (struct AwFmSearchRange){
-      index->kmerSeedTable.table[kmerRangeStartIndex].startPtr - numMatchesInSequenceEnding,
-      index->kmerSeedTable.table[kmerRangeEndIndex].endPtr};
-  }
-  else{
-    return index->kmerSeedTable.table[kmerTableIndex];
-  }
+  return index->kmerSeedTable.table[kmerTableIndex];
 }
 
 
 struct AwFmSearchRange awFmAminoKmerSeedRangeFromTable(const struct AwFmIndex *restrict const index,
   const char *restrict const kmer, const uint8_t kmerLength){
 
-  const bool kmerIsShorterThanTable   = kmerLength < index->metadata.kmerLengthInSeedTable;
-  const uint8_t kmerSeedStartPosition = kmerIsShorterThanTable? 0: kmerLength - index->metadata.kmerLengthInSeedTable;
-
+  const uint8_t kmerSeedStartPosition = kmerLength - index->metadata.kmerLengthInSeedTable;
   uint64_t kmerTableIndex = 0;
   for(int_fast16_t i = kmerSeedStartPosition; i < kmerLength; i++){
     kmerTableIndex = (kmerTableIndex * AW_FM_AMINO_CARDINALITY) + awFmAsciiAminoAcidToLetterIndex(kmer[i]);
   }
 
-  if(kmerIsShorterThanTable){
-    uint8_t numImplicitAppendedLetters  = index->metadata.kmerLengthInSeedTable - kmerLength;
-    uint64_t kmerRangeStartIndex        = kmerTableIndex;
-    uint64_t kmerRangeEndIndex          = kmerTableIndex + 1;
-
-    for(uint8_t i = 0; i < numImplicitAppendedLetters; i++){
-      kmerRangeStartIndex *= AW_FM_AMINO_CARDINALITY;
-      kmerRangeEndIndex   *= AW_FM_AMINO_CARDINALITY;
-    }
-    kmerRangeEndIndex--;
-    uint8_t numMatchesInSequenceEnding = kmerMatchesInSequenceEnding(index, kmerRangeStartIndex, kmerLength);
-
-    return (struct AwFmSearchRange){
-      index->kmerSeedTable.table[kmerRangeStartIndex].startPtr - numMatchesInSequenceEnding,
-      index->kmerSeedTable.table[kmerRangeEndIndex].endPtr};
-  }
-  else{
-    return index->kmerSeedTable.table[kmerTableIndex];
-  }
-}
-
-
-uint8_t kmerMatchesInSequenceEnding(const struct AwFmIndex *restrict const index,
-  const uint64_t kmerIndexEncoding, const uint8_t kmerLength){
-
-  const uint64_t *restrict const sequenceEndingKmerEncodings = index->kmerSeedTable.sequenceEndingKmerEncodings;
-  uint8_t positionsToCheckInKmerTable = index->metadata.kmerLengthInSeedTable - kmerLength;
-  uint8_t matchesFoundInSequenceEnding = 0;
-  for(uint8_t i = 0; i < positionsToCheckInKmerTable; i++){
-    matchesFoundInSequenceEnding += (sequenceEndingKmerEncodings[i] == kmerIndexEncoding);
-  }
-
-  return matchesFoundInSequenceEnding;
+  return index->kmerSeedTable.table[kmerTableIndex];
 }
--- src/AwFmKmerTable.h
+++ src/AwFmKmerTable.h
@@ -11,11 +11,9 @@
  * Function:  awFmNucleotideKmerSeedRangeFromTable
  * --------------------
  * Given an ascii nucleotide kmer, queries the kmerSeedTable for the partially completed range in the backward suffix array,
- *  and returns a pointer to the range of that kmer. This function looks up the range for the
+ *  and returns a copy of that range. This function looks up the range for the
  *  suffix of length equal to the kmers stored in the seed table. As such, using this to query for kmers
  *  that are shorter than those in the table is undefined behavior.
- *
- *  While this function doesn't return the actual value, it does prefetch the value.
  *
  *  Inputs:
  *    index: AwFmIndex struct to search
@@ -24,7 +22,7 @@
  *      where the suffix occurrs in the kmer string.
  *
  *  Returns:
- *    pointer to an AwFmSearchRange containing the startPtr and endPtr for the kmer seed.
+ *    Copy of the AwFmSearchRange containing the startPtr and endPtr for the kmer seed.
  */
 struct AwFmSearchRange awFmNucleotideKmerSeedRangeFromTable(const struct AwFmIndex *restrict const index,
   const char *restrict const kmer, const uint8_t kmerLength);
@@ -34,11 +32,9 @@ struct AwFmSearchRange awFmNucleotideKmerSeedRangeFromTable(const struct AwFmInd
  * Function:  awFmAminoKmerSeedRangeFromTable
  * --------------------
  * Given an ascii amino acid kmer, queries the kmerSeedTable for the partially completed range in the backward suffix array,
- *  and returns a pointer to the range of that kmer. This function looks up the range for the
+ *  and returns a copy of that range. This function looks up the range for the
  *  suffix of length equal to the kmers stored in the seed table. As such, using this to query for kmers
  *  that are shorter than those in the table is undefined behavior.
- *
- *  While this function doesn't return the actual value, it does prefetch the value.
  *
  *  Inputs:
  *    index: AwFmIndex struct to search
@@ -47,15 +43,10 @@ struct AwFmSearchRange awFmNucleotideKmerSeedRangeFromTable(const struct AwFmInd
  *      where the suffix occurrs in the kmer string.
  *
  *  Returns:
- *    pointer to an AwFmSearchRange containing the startPtr and endPtr for the kmer seed.
+ *    Copy of the AwFmSearchRange containing the startPtr and endPtr for the kmer seed.
  */
 struct AwFmSearchRange awFmAminoKmerSeedRangeFromTable(const struct AwFmIndex *restrict const index,
   const char *restrict const kmer, const uint8_t kmerLength);
 
-
-//Added for testing ONLY, not to be included in final release
-//TODO: remove this function prototype
-uint8_t kmerMatchesInSequenceEnding(const struct AwFmIndex *restrict const index,
-  const uint64_t kmerIndexEncoding, const uint8_t kmerLength);
 
 #endif /* end of include guard: AW_FM_KMER_TABLE_H */

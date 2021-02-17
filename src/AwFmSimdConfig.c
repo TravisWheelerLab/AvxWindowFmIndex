0df87a62e372053ed4728858998c84aea1c318f1
Tim Anderson <tim.anderson@umontana.edu>
2021-02-16 18:32:38 -0700

    update SimdConfig to fully support neon SIMD intrinsics

---
+++ src/AwFmSimdConfig.c
@@ -0,0 +1,85 @@
+#include "AwFmSimdConfig.h"
+
+#ifdef AW_FM_SIMD_CONFIG_ARM_NEON
+
+  AwFmSimdVec256 AwFmSimdVecLoad(const AwFmSimdVec256* mem_addr){
+    AwFmSimdVec256 loadVector;
+    loadVector.lowVec = vld1q_u8(mem_addr);
+    loadVector.highVec = vld1q_u8(mem_addr+16); //each lane is 16B, so 2nd has offset of 16
+    return loadVector;
+  }
+
+  AwFmSimdVec256 AwFmSimdVecAnd(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2){
+    AwFmSimdVec256 resultVector;
+    resultVector.lowVec   = vandq_u8(v1.lowVec,  v2.lowVec);
+    resultVector.highVec  = vandq_u8(v1.highVec, v2.highVec);
+    return resultVector;
+  }
+
+  AwFmSimdVec256 AwFmSimdVecOr(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2){
+    AwFmSimdVec256 resultVector;
+    resultVector.lowVec   = vorrq_u8(v1.lowVec,  v2.lowVec);
+    resultVector.highVec  = vorrq_u8(v1.highVec, v2.highVec);
+    return resultVector;
+  }
+
+  AwFmSimdVec256 AwFmSimdVecAndNot(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2){
+    AwFmSimdVec256 resultVector;
+    resultVector.lowVec   = vandq_u8(vmvnq_u8(v1.lowVec),  v2.lowVec);
+    resultVector.highVec  = vandq_u8(vmvnq_u8(v1.highVec), v2.highVec);
+    return resultVector;
+  }
+
+  uint32_t AwFmMaskedVectorPopcount(const AwFmSimdVec256 vec, const uint8_t localQueryPosition){
+    uint64_t bitmasks[4] = {0};
+    uint8_t  bitmaskedQuadWordIndex = localQueryPosition / 64;
+
+    for(int8_t i = 0; i < bitmaskedQuadWordIndex; i++){
+        bitmasks[i] = ~0UL;
+    }
+    bitmasks[bitmaskedQuadWordIndex] = ~0UL >> (63 - (localQueryPosition % 64));
+
+    uint8x16_t lowVectorPopcnt  = vcntq_u8(vec.lowVec);
+    uint8x16_t highVectorPopcnt = vcntq_u8(vec.highVec);
+    uint8_t lowHorizontalSum    = vaddvq_u8(lowVectorPopcnt);
+    uint8_t highHorizontalSum   = vaddvq_u8(highVectorPopcnt);
+
+    return (uint32_t) lowHorizontalSum + (uint32_t) highHorizontalSum;
+  }
+
+#else
+
+  AwFmSimdVec256 AwFmSimdVecLoad(const AwFmSimdVec256* mem_addr){
+      return _mm256_load_si256(mem_addr);
+  }
+
+  AwFmSimdVec256 AwFmSimdVecAnd(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2){
+    return _mm256_and_si256(v1, v2);
+  }
+
+  AwFmSimdVec256 AwFmSimdVecOr(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2){
+    return _mm256_or_si256(v1, v2);
+  }
+
+  AwFmSimdVec256 AwFmSimdVecAndNot(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2){
+    return _mm256_andnot_si256(v1, v2);
+  }
+
+  uint32_t AwFmMaskedVectorPopcount(const AwFmSimdVec256 vec, const uint8_t localQueryPosition){
+    uint64_t bitmasks[4] = {0};
+    uint8_t  bitmaskedQuadWordIndex = localQueryPosition / 64;
+
+    for(int8_t i = 0; i < bitmaskedQuadWordIndex; i++){
+        bitmasks[i] = ~0UL;
+    }
+    bitmasks[bitmaskedQuadWordIndex] = ~0UL >> (63 - (localQueryPosition % 64));
+
+    uint32_t popcount = _mm_popcnt_u64(_mm256_extract_epi64(vec, 0) & bitmasks[0]);
+     popcount +=        _mm_popcnt_u64(_mm256_extract_epi64(vec, 1) & bitmasks[1]);
+     popcount +=        _mm_popcnt_u64(_mm256_extract_epi64(vec, 2) & bitmasks[2]);
+     popcount +=        _mm_popcnt_u64(_mm256_extract_epi64(vec, 3) & bitmasks[3]);
+
+    return popcount;
+  }
+
+#endif
--- src/AwFmSimdConfig.h
+++ src/AwFmSimdConfig.h
@@ -3,63 +3,27 @@
 
 #include <stdint.h>
 
-#ifdef AW_FM_SIMD_CONFIG_M1
-  //M1 config that uses AVX to emulate AVX2
-  #include <emmintrin.h>
-  #include <smmintrin.h>
 
-  typedef struct __m256i{
-    __m128i lowVector;
-    __m128i highVector;
-  } __m256i;
+// define the Simd vector type, which is determined by the architecture we're building for.
+#ifdef AW_FM_SIMD_CONFIG_ARM_NEON
+  #include <arm_neon.h>
 
-  //emulates the AVX2 load instruction
-  __m256i _mm256_load_si256 (__m256i const * mem_addr){
-    __m256i loadVector;
-    loadVector.lowVector = _mm_load_si128(mem_addr);
-    loadVector.highVector = _mm_load_si128(mem_addr + sizeof(__mm128i))
-    return loadVector;
-  }
-
-
-  //emulates the AVX2 AND instruction
-  __m256i _mm256_and_si256(const __m256i v1, const __m256i v2){
-    __m256i andVector;
-    andVector.lowVector   = _mm_and_si128(v1.lowVector, v2.lowVector);
-    andVector.highVector  = _mm_and_si128(v1.highVector, v2.highVector);
-    return andVector;
-  }
-
-  //emulates the AVX2 ANDNOT instruction
-  __m256i _mm256_andnot_si256(const __m256i v1, const __m256i v2){
-    __m256i andNotVector;
-    andNotVector.lowVector   = _mm_andnot_si128(v1.lowVector, v2.lowVector);
-    andNotVector.highVector  = _mm_andnot_si128(v1.highVector, v2.highVector);
-    return andNotVector;
-  }
-
-  //emulates the AVX2 OR instruction
-  __m256i _mm256_or_si256(const __m256i v1, const __m256i v2){
-    __m256i orVector;
-    orVector.lowVector   = _mm_andnot_si128(v1.lowVector, v2.lowVector);
-    orVector.highVector  = _mm_andnot_si128(v1.highVector, v2.highVector);
-    return orVector;
-  }
-
-  //emulates the AVX2 extract function
-  inline __int64 _mm256_extract_epi64(const __m256i vec, const int index){
-    if(index < 2){
-      return _mm_extract_epi64(vec.lowVector, index & 1);
-    }
-    else{
-      return _mm_extract_epi64(vec.highVector, index & 1);
-    }
-  }
+  typedef struct AwFmSimdVec256{
+    uint8x16_t lowVec;
+    uint8x16_t highVec;
+  } AwFmSimdVec256;
 
 #else
-  //if no SIMD configuration was given, attempt to build as normal(AVX2 support)
   #include <immintrin.h>
 
+  typedef __m256i AwFmSimdVec256;
 #endif
+
+
+AwFmSimdVec256 AwFmSimdVecLoad(const AwFmSimdVec256* mem_addr);
+AwFmSimdVec256 AwFmSimdVecAnd(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2);
+AwFmSimdVec256 AwFmSimdVecOr(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2);
+AwFmSimdVec256 AwFmSimdVecAndNot(const AwFmSimdVec256 v1, const AwFmSimdVec256 v2);
+uint32_t AwFmMaskedVectorPopcount(const AwFmSimdVec256 vec, const uint8_t localQueryPosition);
 
 #endif

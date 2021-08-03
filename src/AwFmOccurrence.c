768daaf956972b10f689824999e0bc3d5b2ab857
Tim Anderson <tim.anderson@umontana.edu>
2021-08-03 15:37:33 -0600

    remove deprecated functions

--- src/AwFmOccurrence.c
+++ src/AwFmOccurrence.c
@@ -111,59 +111,6 @@ AwFmSimdVec256 awFmMakeAminoAcidOccurrenceVector(const struct AwFmAminoBlock *re
 
 
 
-// inline uint16_t awFmVectorPopcount(const AwFmSimdVec256 occurrenceVector, const uint8_t localQueryPosition){
-//   return awFmVectorPopcountBuiltin(occurrenceVector, localQueryPosition);
-  //deprecated
-// const AwFmSimdVec256 lowBitsLookupTable = _mm256_set_epi8( 4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0, 4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0);
-// const AwFmSimdVec256 highBitsLookupTable = _mm256_set_epi8(-5,-4,-4,-3,-4,-3,-3,-2,-4,-3,-3,-2,-3,-2,-2, -1,-5,-4,-4,-3,-4,-3,-3,-2,-4,-3,-3,-2,-3,-2,-2, -1);
-// const AwFmSimdVec256 lowNybbleBitmasked = AwFmSimdVecAnd(occurrenceVector, _mm256_set1_epi8(0x0F));
-// const AwFmSimdVec256 lowNybbleBitCount = _mm256_shuffle_epi8(lowBitsLookupTable, lowNybbleBitmasked);
-// const AwFmSimdVec256 highNybbleBits = _mm256_srli_epi16(occurrenceVector, 4);
-// const AwFmSimdVec256 highNybbleBitmasked = AwFmSimdVecAnd(highNybbleBits, _mm256_set1_epi8(0x0F));
-// const AwFmSimdVec256 highNybbleNegativeBitCount = _mm256_shuffle_epi8(highBitsLookupTable, highNybbleBitmasked);
-// const AwFmSimdVec256 sadCountVector = _mm256_sad_epu8(lowNybbleBitCount, highNybbleNegativeBitCount);
-//
-// const uint8_t finalSum = 224 -
-//   ((_mm256_extract_epi8(sadCountVector, 0)) + (_mm256_extract_epi8(sadCountVector, 8)) +
-//   (_mm256_extract_epi8(sadCountVector, 16)) + (_mm256_extract_epi8(sadCountVector, 24)));
-// //224 = 256 - 32, where the 32 is from subtracting one extra from each in the highBitsLookupTable
-// // keeping all the high bitmask one less than actual forces every add to overflow, making this consistent.
-//
-// return finalSum;
-// }
-
-
-// inline uint16_t awFmVectorPopcountBuiltin(const AwFmSimdVec256 occurrenceVector, const uint8_t localQueryPosition){
-//   uint64_t bitmasks[4] = {0};
-//   uint8_t  bitmaskedQuadWordIndex = localQueryPosition / 64;
-//
-//   for(int8_t i = 0; i < bitmaskedQuadWordIndex; i++){
-//       bitmasks[i] = ~0UL;
-//   }
-//   bitmasks[bitmaskedQuadWordIndex] = ~0UL >> (63 - (localQueryPosition % 64));
-//
-//   uint16_t popcount = _mm_popcnt_u64(_mm256_extract_epi64(occurrenceVector, 0) & bitmasks[0]);
-//    popcount +=        _mm_popcnt_u64(_mm256_extract_epi64(occurrenceVector, 1) & bitmasks[1]);
-//    popcount +=        _mm_popcnt_u64(_mm256_extract_epi64(occurrenceVector, 2) & bitmasks[2]);
-//    popcount +=        _mm_popcnt_u64(_mm256_extract_epi64(occurrenceVector, 3) & bitmasks[3]);
-//
-//   return popcount;
-// }
-
-
-
-  //alternative:
-  // uint8_t unalignedBuffer[32 + 31];
-  // uint8_t *alignedBuffer = (uint8_t *)((intptr_t)( unalignedBuffer + 31 ) & ~31);
-  // _mm256_store_si256((AwFmSimdVec256*)alignedBuffer, vec);
-  //
-  // uint64_t *bufferAs64_t = (uint64_t*) alignedBuffer;
-  // return _mm_popcnt_u64(bufferAs64_t[0]) +
-  // _mm_popcnt_u64(bufferAs64_t[1]) +
-  // _mm_popcnt_u64(bufferAs64_t[2]) +
-  // _mm_popcnt_u64(bufferAs64_t[3]);
-// }
-
 
 inline void awFmBlockPrefetch(const void *restrict const baseBlockListPtr, const uint64_t blockByteWidth,
   const uint64_t nextQueryPosition){
@@ -177,28 +124,6 @@ inline void awFmBlockPrefetch(const void *restrict const baseBlockListPtr, const
   }
 }
 
-
-// inline uint8_t awFmGetLetterAtBwtPosition(const union AwFmBwtBlockList blockList,
-//   const enum AwFmAlphabetType alphabet, const uint64_t bwtPosition){
-//   const size_t blockIndex      = awFmGetBlockIndexFromGlobalPosition(bwtPosition);
-//   const size_t positionInBlock = awFmGetBlockQueryPositionFromGlobalPosition(bwtPosition);
-//   const uint8_t byteInBlock    = positionInBlock / 8;
-//   const uint8_t bitInBlockByte = positionInBlock / 8;
-//   uint8_t letter = 0;
-//   uint8_t letterBitWidth = alphabet == AwFmAlphabetNucleotide? 2: 5;
-//   const AwFmSimdVec256 *restrict const letterBitVectorPtr = alphabet == AwFmAlphabetNucleotide?
-//     blockList.asNucleotide[blockIndex].letterBitVectors:
-//     blockList.asAmino[blockIndex].letterBitVectors;
-//
-//   for(uint8_t letterBit = letterBitWidth; letterBit >= 0; letterBit--){
-//     const uint8_t *restrict const blockVectorAsByteArray = (uint8_t*)(letterBitVectorPtr +letterBit);
-//     const uint8_t bit = (blockVectorAsByteArray[byteInBlock] >> bitInBlockByte) & 1;
-//
-//     letter = (letter << 1) | bit;
-//   }
-//
-//   return letter;
-// }
 
 
 /*
@@ -253,47 +178,3 @@ uint8_t awFmGetAminoLetterAtBwtPosition(const struct AwFmAminoBlock *blockPtr, c
 
   return awFmAminoAcidCompressedVectorToLetterIndex(letterAsCompressedVector);
 }
-
-
-// //I might only need to mask one quad-word, then I can just skip popcounting the ones after!
-// //okay, new strategy: compress to bit vector with AVX2. popcount and accumulate up to mask word,
-// //then shift the mask word to zero out as many bits as needed., popcount and add that, then done.
-// //if given position 0, one bit should be set., if given 256, all bits should be set.
-// /*
-//  * Function:  createQueryPositionBitmask
-//  * --------------------
-//  *  Creates an AVX2 vector that acts as a bitmask to remove positions after the query position,
-//  *    and to remove the sentinel character if it exists in this vector.
-//  *
-//  *  Inputs:
-//  *    localQueryPosition: position in this AVX2 vector to query. All bits after
-//  *      this position are cleared in the returned vector.
-//  *
-//  *   Returns:
-//  *     Bitmask Vector for the occurrence vector.
-//  */
-// inline AwFmSimdVec256 createQueryPositionBitmask(const uint8_t localQueryPosition){
-//   //make the mask for the quad word where the localQueryPositions shows up in.
-//   uint64_t quadWordMask = (1UL << ((uint64_t)(localQueryPosition) +1) % 64) - 1;
-//   const uint8_t quadWordIndexForQueryPosition = localQueryPosition / 64;
-//   AwFmSimdVec256 maskVector = _mm256_setzero_si256();
-//
-//   //starting in the quad word where the query position shows up, set the quad word mask,
-//   // and then change the quad word mask to all 1s so everything below gets set to 1s as well.
-//   // this switch-case intentionally falls through, and is supposed to not have 'break' statements.
-//   switch(quadWordIndexForQueryPosition){
-//     case 3:
-//     maskVector = _mm256_insert_epi64(maskVector, quadWordMask, 3);
-//     quadWordMask = -1UL;
-//     case 2:
-//     maskVector = _mm256_insert_epi64(maskVector, quadWordMask, 2);
-//     quadWordMask = -1UL;
-//     case 1:
-//     maskVector = _mm256_insert_epi64(maskVector, quadWordMask, 1);
-//     quadWordMask = -1UL;
-//     case 0:
-//     maskVector = _mm256_insert_epi64(maskVector, quadWordMask, 0);
-//   }
-//
-//   return maskVector;
-// }
